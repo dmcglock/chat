1d5e25c73361c38e2c6fd570afb8e354fd2430b1
Doug McLaughlin <dmcglock@gmail.com>
2020-10-05 23:05:47 -0600

    user creation tests

--- src/test/java/com/dmclaughlin/chat/app/UserServiceTest.java
+++ src/test/java/com/dmclaughlin/chat/app/UserServiceTest.java
@@ -1,15 +1,21 @@
 package com.dmclaughlin.chat.app;
 
-import com.dmclaughlin.chat.app.room.dto.ChatRoomDto;
 import com.dmclaughlin.chat.app.user.dao.UserChatRoomRepo;
+import com.dmclaughlin.chat.app.user.dao.UserDao;
 import com.dmclaughlin.chat.app.user.dao.UserRepo;
 import com.dmclaughlin.chat.app.user.dto.CreateUserDto;
+import com.dmclaughlin.chat.app.user.dto.UserDto;
+import com.dmclaughlin.chat.app.user.exception.UsernameNotUniqueException;
 import com.dmclaughlin.chat.app.user.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class UserServiceTest {
 
@@ -30,7 +36,45 @@ public class UserServiceTest {
   @Test
   public void successfulCreateUser() {
     //given
-    CreateUserDto givenUser = new CreateUserDto(1,2,);
+    CreateUserDto givenUser = CreateUserDto.builder()
+            .firstName("Scott")
+            .lastName("Pilgrim")
+            .username("NegaScott")
+            .build();
+    UserDao savedUser = new UserDao(givenUser.getUsername(), givenUser.getFirstName(), givenUser.getLastName());
+    UserDto result = null;
+
+    //when
+    when(userRepo.save(any(UserDao.class))).thenReturn(savedUser);
+    try {
+      result = userService.createUser(givenUser);
+    } catch (UsernameNotUniqueException e) {
+      //do nothing
+    }
+
+    //then
+    assert result != null;
+    assert result.getFirstName().equals(savedUser.getFirstName());
+    assert result.getLastName().equals(savedUser.getLastName());
+    assert result.getUsername().equals(savedUser.getUsername());
+  }
+
+  @Test
+  public void exceptionOnUserCreation() {
+    //given
+    CreateUserDto givenUser = CreateUserDto.builder()
+            .firstName("Scott")
+            .lastName("Pilgrim")
+            .username("NegaScott")
+            .build();
+
+    //when
+    when(userRepo.save(any(UserDao.class))).thenThrow(UsernameNotUniqueException.class);
+
+    //then
+    Assertions.assertThrows(UsernameNotUniqueException.class, () -> {
+      userService.createUser(givenUser);
+    });
   }
 
 
